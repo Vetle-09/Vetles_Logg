7841a798bc0a8ca9519507c2a657366c0731c97b
Vetle-09 <vetleleanderheide@gmail.com>
2025-09-02 13:35:27 +0200

    UPDATE

---
+++ KonPro/Navn Test.py
@@ -0,0 +1,4 @@
+navn = input("Skriv in navnet ditt takk: ")
+alder = input("Skriv in alderen ditt også takk: ")
+skole = input("Skolen din er da: ")
+print("Hallo, du heter", navn, "og er", alder, "år gammel. Og hvis jeg har rett, ser du dette på", skole)
